9373e96dc91f21aa7b3ced00d0f1a148054005ad
nabilaass <nabilaasshafap@gmail.com>
2023-08-01 22:18:32 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="SISTEM PAKAR DIAGNOSA PENYAKIT HEPATITIS AKUT | www.artificial intelligence.com">
+    <link rel="icon" href="">
+
+    <title>SISTEM PAKAR DIAGNOSA PENYAKIT HEPATITIS AKUT</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/justified-nav.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <?php include "header.php"; ?>
+
+      <main role="main">
+  <div class="row">
+          <div class="col-lg-12">
+            <h2></h2>
+            <p style="text-indent: 45px;">
+            Hepatitis akut, itu merupakan kondisi peradangan pada liver. Secara umum, hepatitis dibagi menjadi dua kelompok, yaitu akut dan kronis, berdasarkan lamanya peradangan dan akibat yang ditimbulkan gangguan hati. Bila lama peradangan atau cedera pada hati berlangsung kurang dari enam bulan, kondisi itu disebut hepatitis akut. Sebaliknya, bila peradangan atau cedera berlangsung lebih dari enam bulan, kondisi itu disebut hepatitis kronis
+</p>
+<p style="text-indent: 45px;">
+Hepatitis akut cukup umum ditemui, menyerang lebih banyak pria dibanding wanita. Kondisi ini dapat terjadi pada pasien dengan usia berapa pun dan dapat ditangani dengan mengurangi faktor-faktor risiko. Jadi, anda tidak perlu jauh-jauh menemui pakar. Berikut ulasannya. </p></br>
+            <p><a class="btn btn-primary" href="diagnosa.php" role="button">Cek Diagnosa &raquo;</a></p>
+          </div>
+        </div>
+
+      </main>
+            </table>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- end Modal Popup -->
+
+      <!-- Site footer -->
+     
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- <script src="js/jquery-slim.min.js"></script> -->
+    <!-- <script>window.jQuery || document.write('<script src="js/jquery.js"><\/script>')</script> -->
+    <script src="js/popper.min.js"></script>
+    <script src="js/jquery-slim.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function(){
+          $("#myModal").modal('show');
+      });
+  </script>
+  </body>
+</html>
